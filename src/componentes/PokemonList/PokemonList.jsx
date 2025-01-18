3bbb46495a23f469bad75cef02f2206990abe60c
Shanu Yadav <yadavshanu023@gmail.com>
2025-01-18 17:13:29 +0530

    Adding pagination

--- src/componentes/PokemonList/PokemonList.jsx
+++ src/componentes/PokemonList/PokemonList.jsx
@@ -5,14 +5,19 @@ import Pokemon from "../Pokemon/Pokemon.jsx";
 function PokemonList() {
     const [pokemonList, setPokemonList] = useState([]);
     const [isLoading, setIsLoading] = useState([true]);
-    const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon';
+    const [pokedexUrl, setPokedexurl] = useState('https://pokeapi.co/api/v2/pokemon');
+    const [nextUrl, setNextUrl] = useState('');
+    const [prevUrl, setPrevUrl] = useState('');
 
     async function downloadPokemons() {
-        const response = await axios.get(POKEDEX_URL); // This downloads list of 20 pokemons
+        setIsLoading(true);
+        const response = await axios.get(pokedexUrl); // This downloads list of 20 pokemons
 
         // we get the array of pokemons from result
         const pokemonResults = response.data.results;
         console.log(response.data);
+        setNextUrl(response.data.next);
+        setPrevUrl(response.data.previous);
 
         // iterating over the array of pokemons, and using their url, to create an array of promises that will download those 20 pokemons
         const pokemonResultsPromis = pokemonResults.map((pokemon) => axios.get(pokemon.url));
@@ -37,7 +42,7 @@ function PokemonList() {
     }
     useEffect(() => {
         downloadPokemons();
-    }, []);
+    }, [pokedexUrl]);
     return (
         <div className="pokemon-list-wrapper">
             <div className="pokemon-wrapper">
@@ -46,8 +51,8 @@ function PokemonList() {
                 }
             </div>
             <div className="controls">
-                <button>Prev</button>
-                <button>Next</button>
+                <button disabled={prevUrl === null} onClick={()=> setPokedexurl(prevUrl)}>Previous</button>
+                <button disabled={nextUrl=== null} onClick={()=> setPokedexurl(nextUrl)}>Next</button>
             </div>
         </div>
     )
